22d8f651fbdd58c1c6b363c497e681a1c53f3d7f
Pawel Paszki <ppaszki@redhat.com>
2018-07-26 15:10:03 +0100

    finished chapter 3

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,26 +1,37 @@
-import React from 'react'
 import '../stylesheets/ui.scss'
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
 
-export const SkiDayCount = React.createClass({
-	render() {
-		return (
-			<div className="ski-day-count">
-				<div className="total-days">
-					<span>{this.props.total}</span>
-					<span>days</span>
-				</div>
-				<div className="powder-days">
-					<span>{this.props.powder}</span>
-					<span>days</span>
-				</div>
-				<div className="backcountry-days">
-					<span>{this.props.backcountry}</span>
-					<span>days</span>
-				</div>
-				<div>
-					<span>{this.props.goal}</span>
-				</div>
-			</div>
-		)
-	}
-})
+const percentToDecimal = (decimal) => {
+	return ((decimal * 100) + '%')
+}
+
+const calcGoalProgress = (total, goal) => {
+	return percentToDecimal(total/goal)
+}
+
+export const SkiDayCount = ({total, powder, backcountry, goal}) => (
+	<div className="ski-day-count">
+		<div className="total-days">
+			<span>{total}</span>
+			<Calendar />
+			<span>days</span>
+		</div>
+		<div className="powder-days">
+			<span>{powder}</span>
+			<SnowFlake />
+			<span>days</span>
+		</div>
+		<div className="backcountry-days">
+			<span>{backcountry}</span>
+				<Terrain />
+			<span>days</span>
+		</div>
+		<div>
+			<span>
+				{calcGoalProgress(total, goal)}
+			</span>
+		</div>
+	</div>
+)
